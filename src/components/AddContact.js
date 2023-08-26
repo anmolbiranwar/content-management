af333d7e2437f8b9a337c1abb185f5c0f75b9692
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-08-25 18:04:51 -0700

    Update AddContact.js

---
+++ src/components/AddContact.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const AddContact = () => {
+  return (
+    <div>
+        <h2>Add Contact</h2>
+        <form className="ui form">
+            <div className="mb-3">
+                <label for="name" className="form-label">Name</label>
+                <input type="text" className="form-control" placeholder='Name'/>
+            </div>
+            <div className="mb-3">
+                <label for="email" className="form-label">Email</label>
+                <input type="text" className="form-control" placeholder='Email'/>
+            </div>
+            <button type="button" class="btn btn-primary">Primary</button>
+        </form>
+    </div>
+  )
+}
+
+export default AddContact;
