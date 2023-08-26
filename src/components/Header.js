ef19c0dd1caa9a0e41b695c000aed769296354e7
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-08-25 18:05:12 -0700

    Update Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='navbar navbar-expand-lg bg-primary'>
+        <div className='fs-2 fw-bold'>
+            <p className='text-center'>Contact Manager</p>
+        </div>
+    </div>
+  );
+};
+
+export default Header;
