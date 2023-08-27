4f3a33389c5a96c974d557de53a7af933ecb13dc
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-08-27 12:44:15 -0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='navbar navbar-expand-lg bg-primary'>
-        <div className='fs-2 fw-bold'>
-            <p className='text-center'>Contact Manager</p>
+    <div className='navbar navbar-expand-lg bg-primary text-center'>
+        <div className='fs-2 fw-bold '>
+            <p>Contact Manager</p>
         </div>
     </div>
   );
