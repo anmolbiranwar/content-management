d9fb1732a02f6d927b2a239420ccb9d312a5cda0
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-08-27 12:42:13 -0700

    Update AddContact.js

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -6,14 +6,14 @@ const AddContact = () => {
         <h2>Add Contact</h2>
         <form className="ui form">
             <div className="mb-3">
-                <label for="name" className="form-label">Name</label>
+                <label htmlFor="name" className="form-label">Name</label>
                 <input type="text" className="form-control" placeholder='Name'/>
             </div>
             <div className="mb-3">
-                <label for="email" className="form-label">Email</label>
+                <label htmlFor="email" className="form-label">Email</label>
                 <input type="text" className="form-control" placeholder='Email'/>
             </div>
-            <button type="button" class="btn btn-primary">Add</button>
+            <button type="button" className="btn btn-primary">Add</button>
         </form>
     </div>
   )
