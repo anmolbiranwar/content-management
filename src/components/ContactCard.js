e453bfd25808ed319360e06357b7b63881c3f295
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-08-29 13:23:27 -0700

    Update ContactCard.js

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import user from "../images/user.png";
 
-const ContactCard = () => {
+const ContactCard = (props) => {
+  const {id, name, email}= props.contact;
   return (
-    <div>ContactCard</div>
-  )
-}
+    <div className="row" style={{marginTop:'20px'}}>
+      <img  src={user} alt='userImage' style={{width:'80px'}}/>
+         <div className="col-sm-5">
+            <dt>{name}</dt>
+            <dd>{email}</dd>
+         </div>
+         <i className="col-sm-5 text-end bi bi-trash fa-lg" style={{color:'red'}}></i>
+         <hr />
+        </div>
+  );
+};
 
 export default ContactCard;
