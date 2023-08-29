a7fa4088455395284349e05f730c45eb790a552a
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-08-29 13:23:31 -0700

    Update ContactList.js

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,19 +1,10 @@
 import React from 'react'
-
+import ContactCard from './ContactCard';
 
 const ContactList = (props) => {
   const renderContactList=props.contacts.map((contact)=>{
     return(
-        <div className="row">
-         <div className="col-sm-3">
-            <dt>{contact.name}</dt>
-            <dd>{contact.email}</dd>
-         </div>
-         <div className="col-sm-9 text-end">
-            <h3><i className="bi bi-trash fa-lg "></i></h3>
-         </div>
-         <hr></hr>
-        </div>
+        <ContactCard contact={contact}/>
     );
   });
   return (
